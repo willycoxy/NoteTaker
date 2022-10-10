ab9b2501cdb56af7eca529ede5a1383b7270320f
willycoxy <williamjwcox@gmail.com>
2022-10-10 00:24:12 -0400

    desceiv

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,30 +3,55 @@ const fs = require('fs')
 const router = require('express').Router();
 // const {notes} = require('../../db/db.json')
 
+// router.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../../db/db.json"))
+// })
+
+
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../db/db.json"))
-})
+    res.sendFile(path.join(__dirname, "../../db/db.json"));
+    
+  });
 
-router.get('/notes/:id', (req,res) => {
-    const notesbyid = findById(req.params.id, notes)
-    if (notesbyid) {
-        res.json(notesbyid)
-    } else {
-        console.log("id not found")
-        res.send(404);
-    }
-})
+  // Client fetches from /notes/:id route
+  router.get("/notes/:id", (req, res) => {
+    let notesInFile = JSON.parse(fs.readFileSync("../../db/db.json", "utf8"))
+    res.json(notesInFile[Number(req.params.id)]);
+    
+  });
 
-router.post('/notes',(req, res) =>{
+// router.get('/notes/:id', (req,res) => {
+//     const notesbyid = findById(req.params.id, notes)
+//     if (notesbyid) {
+//         res.json(notesbyid)
+//     } else {
+//         console.log("id not found")
+//         res.send(404);
+//     }
+// })
 
-    let notespost = JSON.parse(fs.readFileSync(
-    path.join(__dirname, "../../db/db.json"), "utf8"));
-  req.body.id = notespost.length.toSring();
+router.post("/notes", (req, res) => {
+    // set id based on what the next index of the array will be
+    let notesInFile = JSON.parse(fs.readFileSync(
+        // path.join(__dirname, "../../db/db.json"), "utf8"));
+        path.join(__dirname, "../../db/db.json"), "utf8"));
+    req.body.id = notesInFile.length.toString();
+    
+    // Writes/adds to db.json
+    notesInFile.push(req.body);
+    fs.writeFileSync("db/db.json", JSON.stringify(notesInFile));
+      res.json(notesInFile);
+  });
+// router.post('/notes',(req, res) =>{
 
-  notesInFile.push(req.body);
-  fs.writeFileSync("db/db.json", JSON.stringify(notesInFile));
-    res.json(notesInFile);
-})
+//     let notespost = JSON.parse(fs.readFileSync(
+//     path.join(__dirname, "../../db/db.json"), "utf8"));
+//   req.body.id = notespost.length.toSring();
+
+//   notesInFile.push(req.body);
+//   fs.writeFileSync("db/db.json", JSON.stringify(notesInFile));
+//     res.json(notesInFile);
+// })
 
 // router.delete('notes/id', (req,res) =>{
 
