20851ed0cc1f7d815133d5316a1d9a998ac712ae
willycoxy <williamjwcox@gmail.com>
2022-10-10 00:12:32 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const fs = require('fs')
 const apiRoutes = require("./routes/apiRoutes/index");
-const htmlRoutes = require("./routes/htmlRoutes/inex");
+const htmlRoutes = require("./routes/htmlRoutes/index");
 // const util = require("util");
 
 const PORT = process.env.PORT || 3001;
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
